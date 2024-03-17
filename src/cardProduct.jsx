f4ec38fc1986428d2f41e4a1715616822959905f
SebahattinDemir <info.sebahattindemir@gmail.com>
2024-03-17 22:45:24 +0300

    discount-rating

--- src/cardProduct.jsx
+++ src/cardProduct.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
-import Rating from './rating';
-
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import Rating from "./rating";
 
 const CardProduct = (props) => {
+  const { image, title, desc, price, rating, discountPercentage } = props;
 
-  const { image, title, desc, price,rating } = props;
-
-  const addToCart = () => { alert(`Added ${title} to cart`) }
+  const addToCart = () => {
+    alert(`Added ${title} to cart`);
+  };
 
   return (
     <Card className="text-center">
-              <Card.Body>
-                <Card.Img variant="top" src={`img/${image}`} />
-                <Card.Title className="mt-4">{title}</Card.Title>
-                <Rating rating={rating} />
-                <Card.Text>{desc}</Card.Text>
-                <div>
-                <Card.Subtitle>${price}</Card.Subtitle>
-                <Card.Subtitle>${price}</Card.Subtitle>
-                </div>
-                <Button onClick={addToCart} variant="primary" className="mt-3">Add to cart</Button>
-              </Card.Body>
-            </Card>
-  )
-}
+      <Card.Body>
+        <div className="position-relative">
+          <Card.Subtitle className="position-absolute top-0 rounded-3 p-2  text-light bg-success me-3">
+            -{discountPercentage}%
+          </Card.Subtitle>
+          <Card.Img variant="top" src={`img/${image}`} />
+        </div>
+        <Card.Title className="mt-4">{title}</Card.Title>
+        <Rating rating={rating} />
+        <Card.Text>{desc}</Card.Text>
+        <div className="d-flex justify-content-center">
+          <Card.Subtitle className="m-2 text-success fs-4 text-decoration-line-through text-muted">
+            ${((price * discountPercentage) / 100 + price).toFixed(0)}
+          </Card.Subtitle>
+          <Card.Subtitle className="m-2 fs-4 text-success ">
+            ${price}
+          </Card.Subtitle>
+        </div>
+        <Button onClick={addToCart} variant="primary" className="mt-3">
+          Add to cart
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default CardProduct
+export default CardProduct;
